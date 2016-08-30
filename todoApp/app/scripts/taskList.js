e4fc758f2d1fb09376d03c0e6c222d2a7667ac72
raydecastro <captray108@gmail.com>
2016-08-29 21:23:57 -0700

    feature. created tasklist class.

---
+++ todoApp/app/scripts/taskList.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var love = love || {};
+
+(function(namespace) {
+    namespace.TaskList = TaskList;
+
+    function TaskList() {
+    }
+
+    module.exports = TaskList;
+
+})(love);
